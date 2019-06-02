b0a07fd3ad64bacdcac44c876bd919e93aba6876
Blake Rose <bdecktor@gmail.com>
2019-06-02 21:14:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,8 @@
 class Dog
   def initialize(name)
   @name = name
-end
-  def initialize(breed)
-    @breed = breed
   end
-
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
-  end
+  
 
 
 end
